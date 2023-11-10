8a18626a744e9479422a8af99322ab7cc1e9fc2c
JenniGuerraJG <150312014+JenniGuerraJG@users.noreply.github.com>
2023-11-10 12:16:09 -0500

    Add files via upload

---
+++ 7P1Taller1GuerraJennifer/src/pkg7p1taller1guerrajennifer/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg7p1taller1guerrajennifer;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ESPE
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int a, b, suma, resta, multi, div;
+        System.out.println("Nombre: Jennifer Guerra");
+       System.out.println("Curso: 2do ITIN");
+       System.out.println("Materia: POO");
+       System.out.println("Profesor: Ing. Veronica Martinez");
+        System.out.println("Introduce el primer número:");      
+        a = reader.nextInt();
+        System.out.println("Introduce el segundo número distinto a cero:");      
+        b = reader.nextInt();
+        
+        suma= a+b;
+        resta = a-b;
+        multi = a * b;
+        div= a/b;
+        
+        
+        
+    }
+    
+}
+
+
+  
+
+      
